a432e2942667a67ab75f5e85b4b7dbe81cf97b7b
ted <tedkimemail@yahoo.com>
2023-06-14 05:04:57 -0700

    ...

--- temp_files/python1.py
+++ temp_files/python1.py
@@ -1,5 +1,28 @@
 print('hello world')
 
-a = 5
-b = 10
-print(a+b)
+a = 8
+p = 31
+
+for i in range(1,31):
+    # print(i)
+    # a * i mod p = 1
+    # a =8, p = 31
+    if( ((a * i) % p) == 1 ):
+        print(i)
+
+
+print (((a * 4) % p))
+
+print("---------------------")
+
+a = 8
+p = 11
+
+for i in range(1,11):
+    # print(i)
+    # a * i mod p = 7
+    # a =8, p = 31
+    if( ((a * i) % p) == 7 ):
+        print(i)
+
+print (((a * 5) % p))
