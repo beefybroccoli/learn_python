8523c0533973b0e6531978c382489fac96ed7330
ted <tedkimemail@yahoo.com>
2023-06-21 01:20:44 -0700

    ...

--- read_file.py
+++ read_file.py
@@ -6,3 +6,12 @@ print('counters[1] = ', counters[1])
 counters[1] += 1
 print('counters[1] = ', counters[1])
 print(len(counters))
+
+print('============')
+
+print([(1,2,3) for i in range(10)])
+
+sample_bits = [False] * 10
+print('sample_bits', sample_bits)
+
+print("test")
