0c7b5f3f01aa0a8c25321352d61a3fefd32b0672
ted <tedkimemail@yahoo.com>
2023-06-21 02:18:13 -0700

    ...

--- temp_files/python1.py
+++ temp_files/python1.py
@@ -1,30 +1,49 @@
-print('hello world')
+# print('hello world')
 
-a = 8
-p = 31
+# a = 8
+# p = 31
 
-for i in range(1,31):
-    # print(i)
-    # a * i mod p = 1
-    # a =8, p = 31
-    if( ((a * i) % p) == 1 ):
-        print(i)
+# for i in range(1,31):
+#     # print(i)
+#     # a * i mod p = 1
+#     # a =8, p = 31
+#     if( ((a * i) % p) == 1 ):
+#         print(i)
 
 
-print (((a * 4) % p))
+# print (((a * 4) % p))
 
-print("---------------------")
+# print("---------------------")
 
-a = 8
-p = 11
+# a = 8
+# p = 11
 
-for i in range(1,11):
-    # print(i)
-    # a * i mod p = 7
-    # a =8, p = 31
-    if( ((a * i) % p) == 7 ):
-        print(i)
+# for i in range(1,11):
+#     # print(i)
+#     # a * i mod p = 7
+#     # a =8, p = 31
+#     if( ((a * i) % p) == 7 ):
+#         print(i)
 
-print (((a * 5) % p))
+# print (((a * 5) % p))
 
-print("test")
+# print("test")
+
+list_a = [-1, 5, 2, 3, 4, 8, 9, 14, 10, 23]
+k = 4
+left = list_a[:k]
+right = list_a[k+1:]
+pivot = list_a[k]
+print(left)
+print(right)
+
+print(True if len(left) == 0 else max(left) <= pivot)
+print(True if len(right) == 0 else pivot < min(right))
+
+# print(max(left))
+# print(min(right))
+# print(pivot)
+
+# print(list_a[:0])
+# print(list_a[0+1:])
+# print(max(list_a[:0]))
