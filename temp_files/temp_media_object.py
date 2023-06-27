c2064411711a6e1d0133b265d36b2e57a23d27bb
ted <tedkimemail@yahoo.com>
2023-06-27 01:35:44 -0700

    ...

---
+++ temp_files/temp_media_object.py
@@ -0,0 +1,31 @@
+class MedianMaintainingHeap:
+    def __init__(self):
+        self.hmin = MinHeap()
+        self.hmax = MaxHeap()
+        
+    def satisfies_assertions(self):
+        if self.hmin.size() == 0:
+            assert self.hmax.size() == 0
+            return 
+        if self.hmax.size() == 0:
+            assert self.hmin.size() == 1
+            return 
+        # 1. min heap min element >= max heap max element
+        assert self.hmax.max_element() <= self.hmin.min_element(), f'Failed: Max element of max heap = {self.hmax.max_element()} > min element of min heap {self.hmin.min_element()}'
+        # 2. size of max heap must be equal or one lessthan min heap.
+        s_min = self.hmin.size()
+        s_max = self.hmax.size()
+        assert (s_min == s_max or s_max  == s_min -1 ), f'Heap sizes are unbalanced. Min heap size = {s_min} and Maxheap size = {s_max}'
+    
+    def __repr__(self):
+        return 'Maxheap:' + str(self.hmax) + ' Minheap:'+str(self.hmin)
+    
+    def get_median(self):
+        if self.hmin.size() == 0:
+            assert self.hmax.size() == 0, 'Sizes are not balanced'
+            assert False, 'Cannot ask for median from empty heaps'
+        if self.hmax.size() == 0:
+            assert self.hmin.size() == 1, 'Sizes are not balanced'
+            return self.hmin.min_element()
+        # your code here
+        
