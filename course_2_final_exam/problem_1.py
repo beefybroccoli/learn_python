1e1e887d5b51554ee00fdce971fe310c75103112
ted <tedkimemail@yahoo.com>
2023-08-23 05:16:28 -0700

    ...

--- course_2_final_exam/problem_1.py
+++ course_2_final_exam/problem_1.py
@@ -32,7 +32,7 @@ def traverseTree(root_node):
         temp_list = temp_list + traverseTree(root_node.right)  
     
     return temp_list
-
+    # return [root_node.key] + traverseTree(root_node)
 
 # TODO: Implement the function depthWiseTraverse below
 def depthWiseTraverse(root_node):
@@ -43,11 +43,33 @@ def depthWiseTraverse(root_node):
     # ---------------------------------------------------------------
     # perform BFS on a tree
     # use python list as a FIFO data structure to queue nodes
-    temp_list = []
-    status_bool = True
-    traverse_list = [root_node.key] + traverseTree(root_node)
-    print(f'traverse_list = {traverse_list}')
-    return None
+    visited_node = []
+    visited_key = []
+    queue = []
+    queue.append(root_node)
+    while (len(queue) > 0):
+        # get the first node from queue
+        current_node = queue[0]
+        # delete the first node from queue
+        queue = queue[1:]
+        # append the current_node to list of visited_nodes
+        visited_node.append(current_node)
+        # append the current key to list of visited keys
+        visited_key.append(current_node.key)
+        # append the left child node to queue
+        if current_node.left != None:
+            queue.append(current_node.left)
+        # append the right child node to queue
+        if current_node.right != None:
+            queue.append(current_node.right)
+        # debug
+        # print(f'debug - current_node = {current_node.key}')
+        # for element in queue:
+            # print(f'debug - element = {element.key}')
+        # print("debug - ---------------")
+    
+    return visited_key
+    
 
 def make_tree(insertion_list):
     assert len(insertion_list) > 0
@@ -60,20 +82,22 @@ print('-- Test 1 --')
 # Same as the example above
 tree1 = make_tree([11, 18, 15,  13, 21, 17, 4])
 lst1 = depthWiseTraverse(tree1)
-# print(lst1)
-# assert lst1 == [11, 4, 18, 15, 21, 13, 17]
+print(f'return {lst1}')
+assert lst1 == [11, 4, 18, 15, 21, 13, 17]
+print("")
 
-# print('-- Test 2 --')
+print('-- Test 2 --')
+tree2 = make_tree([3, 1, 2, 4, 6, 7])
+lst2 = depthWiseTraverse(tree2)
+print(lst2)
+assert lst2 == [3, 1, 4, 2, 6, 7]
+print("")
 
-# tree2 = make_tree([3, 1, 2, 4, 6, 7])
-# lst2 = depthWiseTraverse(tree2)
-# print(lst2)
-# assert lst2 == [3, 1, 4, 2, 6, 7]
+print('-- Test 3 --')
+tree3 = make_tree([7, 3, 1, 2, 4, 6, 15, 8, 11, 10, 9])
+lst3 = depthWiseTraverse(tree3)
+print(f'return  {lst3}')
+assert lst3 == [7, 3, 15, 1, 4, 8, 2, 6, 11, 10, 9]
+print("")
 
-# print('-- Test 3 --')
-# tree3 = make_tree([7, 3, 1, 2, 4, 6, 15, 8, 11, 10, 9])
-# lst3 = depthWiseTraverse(tree3)
-# print(lst3)
-# assert lst3 == [7, 3, 15, 1, 4, 8, 2, 6, 11, 10, 9]
-
-# print('All tests passed: 15 points')
+print('All tests passed: 15 points')
