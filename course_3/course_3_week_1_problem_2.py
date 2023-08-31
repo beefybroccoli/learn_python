103a9713274064be47f681d7e905dfcac7ce5b12
ted <tedkimemail@yahoo.com>
2023-08-31 07:51:41 -0700

    ...

--- course_3/course_3_week_1_problem_2.py
+++ course_3/course_3_week_1_problem_2.py
@@ -25,11 +25,11 @@ def polynomial_multiply(a_coeff_list, b_coeff_list):
     # -----------------------------------------------------
     # - Pad the coefficients of $a, b$ with zero coefficients to make up two polynomials of degree $m + n - 2$ (expected size of the result).
     new_length = len(a_coeff_list) + len(b_coeff_list) - 1
-    print(f'new_length = {new_length}')
+    # print(f'new_length = {new_length}')
     padded_a_coeff_list = a_coeff_list + [0]*(new_length-len(a_coeff_list))
     padded_b_coeff_list = b_coeff_list + [0]*(new_length-len(b_coeff_list))
-    print(f'padded_a_coeff_list = {padded_a_coeff_list}')
-    print(f'padded_b_coeff_list = {padded_b_coeff_list}')
+    # print(f'padded_a_coeff_list = {padded_a_coeff_list}')
+    # print(f'padded_b_coeff_list = {padded_b_coeff_list}')
 
     # - Compute FFTs of $[a_0, \ldots, a_{n-1}, 0 , \ldots, 0 ]$ and $[b_0, \ldots, b_{n-1}, 0, \ldots, 0 ]$. 
     fft_padded_a_coeff_list = fft(padded_a_coeff_list)
@@ -37,57 +37,53 @@ def polynomial_multiply(a_coeff_list, b_coeff_list):
 
     # - Multiply the FFT sequences: $[ A_0 \times B_0, \ldots, A_{m+n-2} \times B_{m+n-2}]
     result_multiply = multiply(fft_padded_a_coeff_list, fft_padded_b_coeff_list)
-    print(f'len(result_multiply) = {len(result_multiply)}')
+    # print(f'len(result_multiply) = {len(result_multiply)}')
     result_multiply_list = result_multiply.tolist()
-    print(f'len(result_multiply_list) = {len(result_multiply_list)}')
+    # print(f'len(result_multiply_list) = {len(result_multiply_list)}')
 
     # - Compute the inverse FFT to obtain the polynomial $c(x) = a(x) b(x)$.
-    inverse_result_multiply_list = ifft(result_multiply_list)
+    inverse_result_multiply_list = real(ifft(result_multiply_list))
 
-    return inverse_result_multiply_list
-    
-
-
+    return list(map(lambda x : round(x), inverse_result_multiply_list))
 
 def check_poly(lst1, lst2):
-    print(f'Your code found: {lst1}')
-    print(f'Expected: {lst2}')
+    # print(f'Your code found: {lst1}')
+    # print(f'Expected:        {lst2}')
     assert(len(lst1) == len(lst2)), 'Lists have different lengths'
     for (k,j) in zip(lst1, lst2):
         assert abs(k-j)<= 1E-05, 'Polynomials do not match'
-    print('Passed!')
-print('-------') 
+    # print('Passed!')
+# print('-------') 
 
-print('Test # 1')
+# print('Test # 1')
 # # multiply (1 + x - x^3) with (2 - x + x^2)
 a = [1, 1, 0, -1]
 b = [2, -1, 1]
 c = polynomial_multiply(a,b)
-print(f'c = {c}')
 assert(len(c) == 6)
-print(f'c={c}')
+# print(f'c={c}')
 check_poly(c, [2,1,0,-1,1,-1])
-print('-------')
+# print('-------')
 
 
-print('Test # 2')
+# print('Test # 2')
 # multiply 1 - x + x^2 + 2 x^3 + 3 x^5 with 
 #            -x^2 + x^4 + x^6
 a = [1, -1, 1, 2, 0, 3]
 b = [0, 0, -1, 0, 1, 0, 1]
 c = polynomial_multiply(a,b)
 assert(len(c) == 12)
-print(f'c={c}')
+# print(f'c={c}')
 check_poly(c, [0, 0, -1, 1, 0, -3, 2, -2, 1, 5, 0, 3])
-print('-------')
+# print('-------')
 
-print('Test # 3')
+# print('Test # 3')
 # multiply 1 - 2x^3 + x^7 - 11 x^11
 # with     2 - x^4 - x^6 + x^8
 a = [1, 0, 0, -2, 0, 0, 0, 1, 0, 0, 0, -11]
 b = [2, 0, 0, 0, -1, 0, -1, 0, 1]
 c = polynomial_multiply(a, b)
 assert(len(c) == 20)
-print(f'c={c}')
+# print(f'c={c}')
 check_poly(c, [2, 0, 0, -4, -1, 0, -1, 4, 1, 2, 0, -25, 0, -1, 0, 12, 0, 11, 0, -11])
-print('All tests passed (10 points!)')
+# print('All tests passed (10 points!)')
---
+++ course_3/course_3_week_1_problem_3.py
@@ -0,0 +1,80 @@
+## Problem 3 
+"""
+We are given three subsets of numbers $A, B, C\subseteq \{ 0, \ldots, n\}$. Design an algorithm that runs in worst case time  $\Theta(n \log(n))$ that checks if there exists numbers $n_1, n_2$ in $A,B$, respectively and number $n_3$ in $C$ such that
+$ n_1 + n_2 = n_3$. 
+
+**Hint:** Convert the set $A = \{ n_0, n_1, \ldots, n_k\}$ into the polynomial $p_A(x):\ x^{n_0} + x^{n_1} + \cdots + x^{n_k}$. Suppose $p_A(x), p_B(x)$ are polynomials obtained from the sets $A, B$ respectively, interpret what the product  $p_A(x) \times p_B(x)$ signifies. Use this to complete an algorithm for the problem at hand that runs in $n \log(n)$ time. 
+"""
+from numpy import fft, multiply
+from course_3_week_1_problem_2 import polynomial_multiply
+
+# inputs sets a, b, c
+# return True if there exist n1 in a, n2 in B such that n1+n2 in C
+# return False otherwise
+# number n which signifies the maximum number in a, b, c
+# here is a useful reference to set data structure in python
+# https://docs.python.org/3/tutorial/datastructures.html#sets
+def check_sum_exists(a, b, c, n):
+    a_coeffs = [0]*n
+    b_coeffs = [0]*n 
+    # convert sets a, b into polynomials as provided in the hint
+    # a_coeffs and b_coeffs should contain the result
+        
+    # multiply them together
+    print ("a_coeffs . ", a_coeffs)
+    print ("b_coeffs . ", b_coeffs)
+    c_coeffs = polynomial_multiply(a_coeffs, b_coeffs)
+    print("Co-efficients of the testcase are")
+    coeffs_copy = []
+    for num in c_coeffs:
+        if(abs(num-0) < abs(num-1)):
+            coeffs_copy.append(0)
+        elif(abs(num-1) < abs(num-2)):
+            coeffs_copy.append(1)
+        else:
+            coeffs_copy.append(2)
+    print("coeffs_copy= ", coeffs_copy)
+    # use the result to solve the problem at hand
+    # your code here
+    # ------------------------------------------------------------
+    a_multiply_b = polynomial_multiply(list(a),list(b))
+    print(f'a = {a}')
+    print(f'b = {b}')
+    print(f'a_multiply_b = {a_multiply_b}')
+    print(f'c = {c}')
+    return None
+    # return True/False
+
+
+print('-- Test 1 --')
+a = set([1, 2, 10, 11])
+b = set([2, 5, 8, 10])
+c = set([1, 2, 5,  8])
+print(f'check_sum_exists(a, b, c, 12) return {check_sum_exists(a, b, c, 12)}\n')
+# assert not check_sum_exists(a, b, c, 12), f'Failed Test 1: your code returned true when the expected answer is false'
+# print('Passed')
+
+print('-- Test 2 --')
+a = set([1, 2, 10, 11])
+b = set([2, 5, 8, 10])
+c = set([1, 2, 5,  8, 11])
+print(f'check_sum_exists(a, b, c, 12) return {check_sum_exists(a, b, c, 12)}\n')
+# assert check_sum_exists(a, b, c, 12), f'Failed Test 2: your code returns false but note that 1 in a + 10 in b = 11 in c '
+# print('Passed')
+
+# print('-- Test 3 --')
+print(len({1, 4, 5, 7, 11, 13, 14, 15, 17, 19, 22, 23, 24, 28, 34, 35, 37, 39, 42, 44}))
+# a={1, 4, 5, 7, 11, 13, 14, 15, 17, 19, 22, 23, 24, 28, 34, 35, 37, 39, 42, 44}
+# b={0, 1, 4, 9, 10, 11, 12, 15, 18, 20, 25, 31, 34, 36, 38, 40, 43, 44, 47, 49}
+# c={3, 4, 5, 7, 8, 10, 19, 20, 21, 24, 31, 35, 36, 37, 38, 39, 42, 44, 46, 49}
+# print(f'check_sum_exists(a, b, c, 50) return {check_sum_exists(a, b, c, 50)}\n')
+# assert check_sum_exists(a, b, c, 50), f'Failed Test 3: your code returns False whereas the correct answer is true eg., 4 + 0 = 4'
+
+# print('-- Test 4 --')
+
+# a={98, 2, 99, 40, 77, 79, 87, 88, 89, 27}
+# b={64, 66, 35, 69, 70, 40, 76, 45, 12, 60}
+# c={36, 70, 10, 44, 15, 16, 83, 20, 84, 55}
+# assert not check_sum_exists(a, b, c, 100), f'Failed Test 4: your code returns True whereas the correct answer is False'
+
+# print('All Tests Passed (15 points)!')
