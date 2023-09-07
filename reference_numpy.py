ad0625ea3e97d760ef00a837d068d5994258abc6
ted <tedkimemail@yahoo.com>
2023-09-07 06:25:56 -0700

    ...

--- reference_numpy.py
+++ reference_numpy.py
@@ -17,3 +17,24 @@ arr = np.arange(10)**2
 print(f'\narr = {arr}')
 print(f'arr + arr return {arr + arr}')
 print(f'arr + 1   return {arr + 1}')
+
+arr1 = np.array([1,2,3,4,5])
+arr2 = np.array([1,2,3,4,5])
+print(f'\nnp.multiply(arr1, arr2) return {np.multiply(arr1, arr2)}')
+print(f'arr1.dot(arr2)          return {arr1.dot(arr2)}')
+
+arr_int = np.arange(1,6)
+arr_floats = np.arange(1.1,1.6,0.1)
+print(f'\narr_int              =    {arr_int}')
+print(f'arr_floats           =      {arr_floats}')
+print(f'arr_int + arr_floats return {arr_int + arr_floats}')
+# print(f'{}')
+print(f'np.hypot(arr_int, arr_floats) return {np.hypot(arr_int, arr_floats)}')
+print(f'np.greater(arr_int, arr_floats) return {np.greater(arr_int, arr_floats)}') # comparison
+print(f'np.ceil(arr_floats) return {np.ceil(arr_floats)}') # get uppder of a number
+print(f'arr_floats.sum() return {arr_floats.sum()}')
+print(f'arr_floats.mean() return {arr_floats.mean()}')
+print(f'arr_floats.std() return {arr_floats.std()}')
+print(f'arr_floats.var() return {arr_floats.var()}')
+print(f'arr_floats.max() return {arr_floats.max()}')
+print(f'arr_floats.min() return {arr_floats.min()}')
